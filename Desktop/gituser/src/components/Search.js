619a3dad1082e7faf04793a2eb5f6e43c765d103
Akaniru Eunice <chieunice114@gmail.com>
2021-12-19 01:47:32 -0800

    created a search input

--- Desktop/gituser/src/components/Search.js
+++ Desktop/gituser/src/components/Search.js
@@ -2,8 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
+import { useState } from 'react';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = useState('')
+  //get thing from global context
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(user){
+      //more logic
+      
+      //optional
+      // setUser('');
+    }
+  }
+  return <section className='section'>
+    <Wrapper className='section-center'>
+      <form onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <MdSearch/>
+          <input type='text' placeholder='enter github user'
+          value={user}
+          onChange={(e) => setUser(e.target.value)} />
+          <button type='submit'>search</button>
+        </div>
+      </form>
+      <h3>requests : 60 / 60</h3>
+    </Wrapper>
+  </section>;
 };
 
 const Wrapper = styled.div`
