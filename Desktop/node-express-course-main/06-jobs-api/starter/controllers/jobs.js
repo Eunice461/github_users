698f11b932cdfb0476e67a5d172435fe735d59aa
Akaniru Eunice <chieunice114@gmail.com>
2022-01-11 23:20:11 -0800

    created the user schema and also the route part

---
+++ Desktop/node-express-course-main/06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,25 @@
+const getAllJobs = async (req, res) => {
+	res.send("get all jobs");
+};
+const getJob = async (req, res) => {
+	res.send("get jobs");
+};
+const createJob = async (req, res) => {
+	res.send("create jobs");
+};
+const updateJob = async (req, res) => {
+	res.send("update jobs");
+};
+const deleteJob = async (req, res) => {
+	res.send("delete jobs");
+};
+
+
+module.exports = {
+	getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+
+};
